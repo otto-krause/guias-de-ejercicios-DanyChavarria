95d13ea082ec9742fc5d675fb08b48f43f328b87
DanyChavarria <danyjuchava12@gmail.com>
2020-04-17 16:01:57 -0300

    Termine el ejercicio 3 de la guia 1

--- Guia 1/E3/Program.cs
+++ Guia 1/E3/Program.cs
@@ -6,7 +6,56 @@ namespace E3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 0;
+            int cont = 0;
+
+            Program.NumeroPar();
+            Program.NumeroPrimo();
+        }
+
+        static void NumeroPar()
+        {
+            int num = 0;
+
+            Console.Write("Ingrese un numero para saber si es par o no: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            if ( num % 2 == 0)
+            {
+                Console.WriteLine("El numero que ingreso es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero que ingreso no es par");
+            }
+        }
+
+        static void NumeroPrimo()
+        {
+            int num = 0;
+            int cont = 0;
+
+            Console.Write("Ingrese un numero para saber si es primo o no: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if ( num % i == 0)
+                {
+                    
+                    cont += 1;
+                }
+            }
+
+            if (cont > 2)
+            {
+                Console.WriteLine("El numero que ingreso no es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero que ingreso es primo");
+            }
+
         }
     }
 }
