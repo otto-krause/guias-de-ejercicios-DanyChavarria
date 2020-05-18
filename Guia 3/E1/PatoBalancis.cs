7626fbcc282bc4c6416556ddd4c615d4d520d881
DanyChavarria <danyjuchava12@gmail.com>
2020-05-17 23:13:28 -0300

    termine ejecicio 1 de la guia 3

---
+++ Guia 3/E1/PatoBalancis.cs
@@ -0,0 +1,14 @@
+namespace E1
+{
+    public class PatoBalancis : Empleado
+    {
+        public PatoBalancis()
+        {
+        }
+
+        public bool estaDistraido()
+        {
+            return false;
+        }
+    }
+}
--- Guia 3/E1/Program.cs
+++ Guia 3/E1/Program.cs
@@ -6,7 +6,56 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Homero homero = new Homero(2);
+            Lenny lenny = new Lenny();
+            PatoBalancis pato = new PatoBalancis();
+            MrBurns burns = new MrBurns();
+            Planta planta = new Planta(10001, homero, burns);
+            
+            string opcion = "";
+            do{
+                Console.WriteLine("1)Ver si la planta esta en peligro\n"+
+                "2)Homero come dona\n"+
+                "3)Homero compra donas\n"+
+                "4)Lenny toma cerveza\n"+
+                "5)Cambiar empleado por Homero\n"+
+                "6)Cambiar el empleado por Lenny\n"+
+                "7)Cambiar el empleado por el Pato\n"+
+                "8)Despojar a burns de sus riquezas");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine(planta.estaEnPeligro());
+                        break;
+                    case "2":
+                        homero.comer();
+                        break;
+                    case "3":
+                        homero.comprar();
+                        break;
+                    case "4":
+                        lenny.tomarCervezas();
+                        break;
+                    case "5":
+                        planta.CambiarEmpleado(homero);
+                        break;
+                    case "6":
+                        planta.CambiarEmpleado(lenny);
+                        break;
+                    case "7":
+                        planta.CambiarEmpleado(pato);
+                        break;
+                    case "8":
+                        burns.despojarDeSusRiquezas();
+                        break;                    
+                    default:
+                        opcion = "salir";
+                        break;
+                }
+            } 
+            while(opcion != "salir");            
         }
     }
 }
